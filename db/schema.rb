9f18f9eecfed5911b26c35fe5a75effaba4d2c8e
shirin238 <cinzia.faenza@gmail.com>
2013-02-22 11:30:41 -0800

    installed devise, add userid to restaurants and activities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130216192204) do
+ActiveRecord::Schema.define(:version => 20130222192550) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(:version => 20130216192204) do
     t.string   "website"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
+
+  add_index "restaurants", ["user_id"], :name => "index_restaurants_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
